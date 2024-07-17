43d4ee6b5e2232a07707702c99cb174de20fa5ee
Julien Dehee <julien.dehee@algolia.com>
2024-07-17 20:24:36 +0200

    add logger

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ from location import generate_all_locations
 from game import Game
 from data import load_deck_data, printwinrate
 
-all_cards = generate_all_cards()
-all_locations = generate_all_locations()
-for i in range (1,1):
-    game = Game()
-    game.play_game()
+from loguru import logger
 
-decks_data = load_deck_data('decks_data.json')
-printwinrate(decks_data)
+def main():
+    logger.info('Started')
+
+    all_cards = generate_all_cards()
+    all_locations = generate_all_locations()
+    for i in range (1,1):
+        game = Game()
+        game.play_game()
+
+    decks_data = load_deck_data('decks_data.json')
+    printwinrate(decks_data)
+
+    logger.info('Finished')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
